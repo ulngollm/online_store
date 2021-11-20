0d574dbb1582a35fd9f2ae9435d64aaf18e8ca37
ulngollm <ulngollm@gmail.com>
2021-11-20 18:24:00 +0300

    add order tables

---
+++ database/migrations/2021_11_20_131818_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->tinyInteger('status_id')->unsigned();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
